5a76b42c8c661d0b9d7899340ba4f33cd35e5aa4
Santhoshi Pravallika Machagiri(UST <285484@ust.com>
2024-08-27 12:56:08 +0530

    Initialise

--- appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
@@ -2,10 +2,13 @@ package com.healthcare.appointment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients // Enable Feign client
+@EnableFeignClients
+@EnableDiscoveryClient
+// Enable Feign client
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
